92d54d4a6293e1a09208ebe6c895906796d803e4
JosephWetzel <joe.m.wetzel@gmail.com>
2013-04-25 13:18:50 -0500

    Adding all projects files

---
+++ CaptureTheFlag/Main.Designer.vb
@@ -0,0 +1,159 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.tmrRunner = New System.Windows.Forms.Timer(Me.components)
+        Me.lblControl = New System.Windows.Forms.Label()
+        Me.lblBlue = New System.Windows.Forms.Label()
+        Me.SplitBottom = New System.Windows.Forms.SplitContainer()
+        Me.lblGreen = New System.Windows.Forms.Label()
+        Me.lblYellow = New System.Windows.Forms.Label()
+        Me.lblRed = New System.Windows.Forms.Label()
+        CType(Me.SplitBottom, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitBottom.Panel1.SuspendLayout()
+        Me.SplitBottom.Panel2.SuspendLayout()
+        Me.SplitBottom.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'tmrRunner
+        '
+        Me.tmrRunner.Enabled = True
+        Me.tmrRunner.Interval = 10000
+        '
+        'lblControl
+        '
+        Me.lblControl.BackColor = System.Drawing.Color.Gray
+        Me.lblControl.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.lblControl.Font = New System.Drawing.Font("Microsoft Sans Serif", 60.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblControl.Location = New System.Drawing.Point(0, 0)
+        Me.lblControl.Name = "lblControl"
+        Me.lblControl.Size = New System.Drawing.Size(902, 408)
+        Me.lblControl.TabIndex = 0
+        Me.lblControl.Text = "No one has current control"
+        Me.lblControl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblBlue
+        '
+        Me.lblBlue.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lblBlue.BackColor = System.Drawing.Color.Gray
+        Me.lblBlue.Font = New System.Drawing.Font("Microsoft Sans Serif", 50.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblBlue.ForeColor = System.Drawing.Color.Blue
+        Me.lblBlue.Location = New System.Drawing.Point(534, 0)
+        Me.lblBlue.Name = "lblBlue"
+        Me.lblBlue.Size = New System.Drawing.Size(368, 76)
+        Me.lblBlue.TabIndex = 0
+        Me.lblBlue.Text = "0"
+        Me.lblBlue.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'SplitBottom
+        '
+        Me.SplitBottom.BackColor = System.Drawing.Color.Gray
+        Me.SplitBottom.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.SplitBottom.Location = New System.Drawing.Point(0, 314)
+        Me.SplitBottom.Name = "SplitBottom"
+        '
+        'SplitBottom.Panel1
+        '
+        Me.SplitBottom.Panel1.Controls.Add(Me.lblGreen)
+        '
+        'SplitBottom.Panel2
+        '
+        Me.SplitBottom.Panel2.Controls.Add(Me.lblYellow)
+        Me.SplitBottom.Size = New System.Drawing.Size(902, 94)
+        Me.SplitBottom.SplitterDistance = 447
+        Me.SplitBottom.TabIndex = 2
+        Me.SplitBottom.TabStop = False
+        '
+        'lblGreen
+        '
+        Me.lblGreen.BackColor = System.Drawing.Color.Gray
+        Me.lblGreen.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.lblGreen.Font = New System.Drawing.Font("Microsoft Sans Serif", 50.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblGreen.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.lblGreen.Location = New System.Drawing.Point(0, 0)
+        Me.lblGreen.Name = "lblGreen"
+        Me.lblGreen.Size = New System.Drawing.Size(447, 94)
+        Me.lblGreen.TabIndex = 0
+        Me.lblGreen.Text = "0"
+        Me.lblGreen.TextAlign = System.Drawing.ContentAlignment.BottomLeft
+        '
+        'lblYellow
+        '
+        Me.lblYellow.BackColor = System.Drawing.Color.Gray
+        Me.lblYellow.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.lblYellow.Font = New System.Drawing.Font("Microsoft Sans Serif", 50.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblYellow.ForeColor = System.Drawing.Color.Yellow
+        Me.lblYellow.Location = New System.Drawing.Point(0, 0)
+        Me.lblYellow.Name = "lblYellow"
+        Me.lblYellow.Size = New System.Drawing.Size(451, 94)
+        Me.lblYellow.TabIndex = 0
+        Me.lblYellow.Text = "0"
+        Me.lblYellow.TextAlign = System.Drawing.ContentAlignment.BottomRight
+        '
+        'lblRed
+        '
+        Me.lblRed.AutoSize = True
+        Me.lblRed.BackColor = System.Drawing.Color.Gray
+        Me.lblRed.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.lblRed.Font = New System.Drawing.Font("Microsoft Sans Serif", 50.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblRed.ForeColor = System.Drawing.Color.Red
+        Me.lblRed.Location = New System.Drawing.Point(0, 0)
+        Me.lblRed.Name = "lblRed"
+        Me.lblRed.Size = New System.Drawing.Size(70, 76)
+        Me.lblRed.TabIndex = 3
+        Me.lblRed.Text = "0"
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(902, 408)
+        Me.Controls.Add(Me.lblRed)
+        Me.Controls.Add(Me.lblBlue)
+        Me.Controls.Add(Me.SplitBottom)
+        Me.Controls.Add(Me.lblControl)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Main"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Main"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.SplitBottom.Panel1.ResumeLayout(False)
+        Me.SplitBottom.Panel2.ResumeLayout(False)
+        CType(Me.SplitBottom, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitBottom.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents tmrRunner As System.Windows.Forms.Timer
+    Friend WithEvents lblControl As System.Windows.Forms.Label
+    Friend WithEvents lblBlue As System.Windows.Forms.Label
+    Friend WithEvents SplitBottom As System.Windows.Forms.SplitContainer
+    Friend WithEvents lblGreen As System.Windows.Forms.Label
+    Friend WithEvents lblYellow As System.Windows.Forms.Label
+    Friend WithEvents lblRed As System.Windows.Forms.Label
+
+End Class
---
+++ CaptureTheFlag/Main.vb
@@ -0,0 +1,232 @@
+﻿Imports System.Text
+Imports System.Security.Cryptography
+Imports System.IO
+
+Public Class Main
+    Dim strControl As String = ""
+    Dim ExtraPoints As Boolean = False
+    Dim ExtraPointTick As Integer = 0
+    Dim ExtraPointColor As String = ""
+
+
+    Private Sub tmrRunner_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmrRunner.Tick
+        Try
+
+            Dim oRead As System.IO.StreamReader
+            oRead = IO.File.OpenText(My.Settings.server)
+            Dim LineIn As String = ""
+            Dim blnFound As Boolean = False
+
+
+            While oRead.Peek <> -1
+                LineIn = oRead.ReadLine()
+                LineIn = DecryptString128Bit(LineIn, My.Settings.key).Trim()
+            End While
+
+            oRead.Close()
+
+
+            If LineIn.Contains("Red") Then
+                blnFound = True
+                If Not strControl = "Red" Then
+                    lblRed.Text = CDbl(lblRed.Text) + 50
+                Else
+                    lblRed.Text = CDbl(lblRed.Text) + 1
+                End If
+                If ExtraPoints And Not ExtraPointColor = "Red" Then
+                    lblRed.Text = CDbl(lblRed.Text) + 700
+                    ExtraPointColor = "Red"
+                End If
+                My.Settings.RedTeam = lblRed.Text
+                My.Settings.Save()
+
+                lblControl.Text = "The Red Team has control"
+                lblControl.ForeColor = Color.Red
+                strControl = "Red"
+                ExtraPointColor = "Red"
+                ExtraPointTick = 0
+                ExtraPoints = False
+            End If
+
+            If LineIn.Contains("Green") Then
+                blnFound = True
+                If Not strControl = "Green" Then
+                    lblGreen.Text = CDbl(lblGreen.Text) + 50
+                Else
+                    lblGreen.Text = CDbl(lblGreen.Text) + 1
+                End If
+                If ExtraPoints And Not ExtraPointColor = "Green" Then
+                    lblGreen.Text = CDbl(lblGreen.Text) + 700
+                    ExtraPointColor = "Green"
+                End If
+                My.Settings.GreenTeam = lblGreen.Text
+                My.Settings.Save()
+                lblControl.Text = "The Green Team has control"
+                lblControl.ForeColor = Color.Green
+                strControl = "Green"
+                ExtraPointColor = "Green"
+                ExtraPointTick = 0
+                ExtraPoints = False
+            End If
+
+
+            If LineIn.Contains("Yellow") Then
+                blnFound = True
+                If Not strControl = "Yellow" Then
+                    lblYellow.Text = CDbl(lblYellow.Text) + 50
+                Else
+                    lblYellow.Text = CDbl(lblYellow.Text) + 1
+                End If
+                If ExtraPoints And Not ExtraPointColor = "Yellow" Then
+                    lblYellow.Text = CDbl(lblYellow.Text) + 700
+                    ExtraPointColor = "Yellow"
+                End If
+                My.Settings.YellowTeam = lblYellow.Text
+                My.Settings.Save()
+                lblControl.Text = "The Yellow Team has control"
+                lblControl.ForeColor = Color.Yellow
+                strControl = "Yellow"
+                ExtraPointColor = "Yellow"
+                ExtraPointTick = 0
+                ExtraPoints = False
+            End If
+
+            If LineIn.Contains("Blue") Then
+                blnFound = True
+                If Not strControl = "Blue" Then
+                    lblBlue.Text = CDbl(lblBlue.Text) + 50
+                Else
+                    lblBlue.Text = CDbl(lblBlue.Text) + 1
+                End If
+                If ExtraPoints And Not ExtraPointColor = "Blue" Then
+                    lblBlue.Text = CDbl(lblBlue.Text) + 700
+                    ExtraPointColor = "Blue"
+                End If
+                My.Settings.BlueTeam = lblBlue.Text
+                My.Settings.Save()
+                lblControl.Text = "The Blue Team has control"
+                lblControl.ForeColor = Color.Blue
+                strControl = "Blue"
+                ExtraPointColor = "Blue"
+                ExtraPointTick = 0
+                ExtraPoints = False
+            End If
+
+            If blnFound = False Then
+                ExtraPointTick += 1
+                If ExtraPointTick > 300 Then
+                    ExtraPoints = True
+                    lblControl.Text = "700 Bonus Points"
+                    lblControl.ForeColor = Color.Cyan
+
+                Else
+                    lblControl.Text = "No one has current control"
+                    lblControl.ForeColor = Color.Black
+                End If
+                
+            End If
+
+        Catch ex As Exception
+            ExtraPointTick += 1
+            If ExtraPointTick > 30 Then
+                ExtraPoints = True
+                lblControl.Text = "700 Bonus Points"
+                lblControl.ForeColor = Color.Cyan
+
+            Else
+                lblControl.Text = "No one has current control"
+                lblControl.ForeColor = Color.Black
+            End If
+        End Try
+    End Sub
+
+
+    Public Function DecryptString128Bit(ByVal vstrStringToBeDecrypted As String, _
+                                        ByVal vstrDecryptionKey As String) As String
+
+        Dim bytDataToBeDecrypted() As Byte
+        Dim bytTemp() As Byte
+        Dim bytIV() As Byte = {121, 241, 10, 1, 132, 74, 11, 39, 255, 91, 45, 78, 14, 211, 22, 62}
+        Dim objRijndaelManaged As New RijndaelManaged()
+        Dim objMemoryStream As MemoryStream
+        Dim objCryptoStream As CryptoStream
+        Dim bytDecryptionKey() As Byte
+
+        Dim intLength As Integer
+        Dim intRemaining As Integer
+        Dim intCtr As Integer
+        Dim strReturnString As String = String.Empty
+        Dim achrCharacterArray() As Char
+        Dim intIndex As Integer
+
+        '   *****************************************************************
+        '   ******   Convert base64 encrypted value to byte array      ******
+        '   *****************************************************************
+
+        bytDataToBeDecrypted = Convert.FromBase64String(vstrStringToBeDecrypted)
+
+        '   ********************************************************************
+        '   ******   Encryption Key must be 256 bits long (32 bytes)      ******
+        '   ******   If it is longer than 32 bytes it will be truncated.  ******
+        '   ******   If it is shorter than 32 bytes it will be padded     ******
+        '   ******   with upper-case Xs.                                  ****** 
+        '   ********************************************************************
+
+        intLength = Len(vstrDecryptionKey)
+
+        If intLength >= 32 Then
+            vstrDecryptionKey = Strings.Left(vstrDecryptionKey, 32)
+        Else
+            intLength = Len(vstrDecryptionKey)
+            intRemaining = 32 - intLength
+            vstrDecryptionKey = vstrDecryptionKey & Strings.StrDup(intRemaining, "X")
+        End If
+
+        bytDecryptionKey = Encoding.ASCII.GetBytes(vstrDecryptionKey.ToCharArray)
+
+        ReDim bytTemp(bytDataToBeDecrypted.Length)
+
+        objMemoryStream = New MemoryStream(bytDataToBeDecrypted)
+
+        '   ***********************************************************************
+        '   ******  Create the decryptor and write value to it after it is   ******
+        '   ******  converted into a byte array                              ******
+        '   ***********************************************************************
+
+        Try
+
+            objCryptoStream = New CryptoStream(objMemoryStream, _
+               objRijndaelManaged.CreateDecryptor(bytDecryptionKey, bytIV), _
+               CryptoStreamMode.Read)
+
+            objCryptoStream.Read(bytTemp, 0, bytTemp.Length)
+
+            objCryptoStream.FlushFinalBlock()
+            objMemoryStream.Close()
+            objCryptoStream.Close()
+
+        Catch
+
+        End Try
+
+        '   *****************************************
+        '   ******   Return decypted value     ******
+        '   *****************************************
+
+        'Return StripNullCharacters(Encoding.ASCII.GetString(bytTemp))
+        Return Encoding.ASCII.GetString(bytTemp)
+
+    End Function
+
+    Private Sub lblControl_DoubleClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles lblControl.DoubleClick
+        End
+    End Sub
+
+    Private Sub Main_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        lblBlue.Text = My.Settings.BlueTeam
+        lblRed.Text = My.Settings.RedTeam
+        lblGreen.Text = My.Settings.GreenTeam
+        lblYellow.Text = My.Settings.YellowTeam
+    End Sub
+
+End Class
